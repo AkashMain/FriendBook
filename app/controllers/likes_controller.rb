d1d95c6fac54f581fff4cdf35ae1af8a0e90ac20
AkashMain <mishra.akash1501@gmail.com>
2023-04-12 16:54:03 +0530

    Posts like as JS

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,25 +1,39 @@
 class LikesController < ApplicationController
   
   def create
+    @posts = Post.all.order(created_at: :desc)
     @post = Post.find(params[:post_id])
     @like = @post.likes.build(user: current_user)
 
     if @like.save 
-      redirect_to posts_path, notice: 'You liked this post'
+      respond_to do |format| 
+        format.html {redirect_to posts_path, notice: 'You liked this post'}
+        format.js
+      end
     else 
-      redirect_to posts_path, notice: 'Sorry, something went wrong'
+      respond_to do |format| 
+        format.html {redirect_to posts_path, notice: 'Sorry, something went wrong'}
+        format.js
+      end 
     end
   end
 
   def destroy
     # binding.pry
+    @posts = Post.all.order(created_at: :desc)
     @post = Post.find(params[:post_id])
     @like = @post.likes.find(params[:id])
 
     if @like.user == current_user && @like.destroy
-      redirect_to posts_path, notice: "You unliked this post."
+      respond_to do |format| 
+        format.html {redirect_to posts_path, notice: "You unliked this post."}
+        format.js
+      end
     else
-      redirect_to posts_path, alert: "Sorry, something went wrong. Please try again."
+      respond_to do |format| 
+        format.html {redirect_to posts_path, alert: "Sorry, something went wrong. Please try again."}
+        format.js 
+      end
     end
   end
 
