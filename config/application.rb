4238339498b1426681b367b38cd282590473ee00
AkashMain <mishra.akash1501@gmail.com>
2023-04-24 16:34:17 +0530

    applying ActiveAdmin

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Friendbook
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.active_job.queue_adapter = :sidekiq
+    config.assets.precompile += %w[active_admin.scss]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
