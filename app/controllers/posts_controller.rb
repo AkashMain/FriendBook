6037451b8f8274ce27d3ffb5eb81efa522b7e189
AkashMain <mishra.akash1501@gmail.com>
2023-05-10 11:09:35 +0530

    Searching Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @posts = Post.all.order(created_at: :desc).paginate(page: params[:page], per_page: 5)
+    @posts = Post.all.searching(params[:search]).order(created_at: :desc).paginate(page: params[:page], per_page: 5)
     # @posts = Post.all.order(created_at: :desc).page(params[:page]).per(10)
     respond_to do |format|
       format.html
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,11 +7,13 @@ class Post < ApplicationRecord
 
   validates :body, presence: true, length: {maximum: 300}
 
-  def liked_by?(user)
-    likes.where(user_id: user.id).exists?
-  end
+  # def self.search(q)
+  #   if q
+  #     where('body LIKE ?', "%#{q}%") 
+  #   else
+  #     all
+  #   end
+  # end
+  scope :searching, ->(query) {where("body LIKE ?", "%#{query}%")}
 
-  def editable_by?(user)
-    user == self.user
-  end
 end
