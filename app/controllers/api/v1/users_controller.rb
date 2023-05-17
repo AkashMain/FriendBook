c87d8c4af186e3efd95a9c0ef7575bb0963c2697
AkashMain <mishra.akash1501@gmail.com>
2023-05-17 13:13:05 +0530

    jwt in create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,33 +5,52 @@ module Api
             skip_before_action :verify_authenticity_token
  
             def index 
-                @users = User.all
-                render json: @users, each_serializer: UserSerializer
+              @users = User.all
+              render json: @users, each_serializer: UserSerializer
             end
             
             def show 
-                @user = User.find(params[:id])
-                @posts = @user.posts.order(created_at: :desc)
-                @friends = @user.friends    
-                render json: {user: @user, posts: @posts, friends: @friends}
+              @user = User.find(params[:id])
+              @posts = @user.posts.order(created_at: :desc)
+              @friends = @user.friends    
+              render json: {user: @user, posts: @posts, friends: @friends}
             end
             
+            # def create
+            #     @user = User.new(user_params)
+            #     if @user.save
+            #       render json: @user, status: :created
+            #     else
+            #       render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+            #     end
+            # end
+
             def create
-                @user = User.new(user_params)
-                if @user.save
-                  render json: @user, status: :created
-                else
-                  render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
-                end
+                # Verify and decode the JWT token
+                # token = request.headers['Authorization'].split(' ')[1]
+                
+                # Access the payload from the decoded token
+                # user_id = decoded_token[0]['user_id']
+                
+              @user = User.create(user_params)
+  
+              if @user.valid?
+                token = encode_token(user_id: @user.id)
+                decoded_token = JWT.decode(token, Rails.application.secrets.secret_key_base)
+        
+                render json: {user: @user, token: token, decoded_token: decoded_token}, status: :created
+              else
+                render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+              end
             end
 
             def update
-                @user = User.find(params[:id])
-                if @user.update(user_params)
-                  render json: @user
-                else
-                  render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
-                end
+              @user = User.find(params[:id])
+              if @user.update(user_params)
+                render json: @user
+              else
+                render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+              end
             end
 
             def destroy 
@@ -41,6 +60,10 @@ module Api
             end
             
             private
+            
+            def encode_token(payload)
+              JWT.encode(payload, Rails.application.secrets.secret_key_base)    
+            end
 
             def user_params
               params.require(:user).permit(:email, :password, :password_confirmation, :fname, :lname, :full_name)
